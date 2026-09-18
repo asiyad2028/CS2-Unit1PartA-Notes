59913114812bdffd7632a3e4f9cc9a31decd47fd
asiyad2028 <asiya.dowdy2028@gbwl.org>
2026-09-18 16:34:52 +0000

    added more comments

--- Main.java
+++ Main.java
@@ -1,4 +1,11 @@
-/* comment space practice */
+/* comment space practice 
+Vocab
+algorithum:a step by step process to accomplish a task
+pseudocode:simplified code to outline programs/algorithum. (ex- add func ()
+                                                                  num 1
+                                                                  num 2
+                                                                  1+2 prints) (this helps us with sequencing-the order of steps)*/
+/* What is the difference between java script and java? They are entirely different languages but they have a shared name due to 1990s marketing. Java is a statically typed, class-based language that compiles to bytecode to run on a Virtual Machine (JVM), making it most useful for Android apps, corporate backends, and enterprise software. JavaScript is a dynamically and weakly typed, prototype-based scripting language that runs natively in web browsers to create interactive websites and full-stack web applications. While Java relies on multi-threading to handle complex, concurrent tasks, JavaScript uses a single-threaded asynchronous event loop to keep web pages responsive.*/
 
 public class Main {
 
